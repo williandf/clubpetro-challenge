1146baf908d99f1cebac3194b6294492ac38b12b
Willian Del Freu <williankeyex@gmail.com>
2021-01-12 15:58:10 -0300

    chore: config module import dotenv file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CardsController } from './cards/cards.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController, CardsController],
   providers: [AppService],
 })
